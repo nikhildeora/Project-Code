af4a24660047de36e4407f313c13f7e0eb97752b
nikhildeora <fitnexy@gmail.com>
2022-09-28 23:39:05 +0530

    home page complete

--- home.js
+++ home.js
@@ -1,7 +1,32 @@
 let form = document.querySelector("form")
-form.addEventListener("submit", closereload)
+// let btn = document.querySelector("button")
+// btn.addEventListener("click", printdata)
 
-function closereload(event){
-    event.preventDefault()
-    
+let abt = document.querySelector("#form>div>a")
+abt.addEventListener("click", printdata)
+
+function printdata() {
+
+    let data = {
+        place: form.location.value,
+        fromDate: form.fromDate.value,
+        toDate: form.toDate.value
+    }
+
+    localStorage.setItem("Round-Trip", JSON.stringify(data))
 }
+
+let openit = document.querySelector("#aboutus>p")
+openit.addEventListener("click", aboutus)
+
+function aboutus() {
+    document.querySelector("#aboutus>ul").style.display = "block"
+}
+
+let closeit = document.querySelector("#aboutus>i")
+closeit.addEventListener("click", closebox)
+
+function closebox() {
+    document.querySelector("#aboutus>ul").style.display = "none"
+}
+
