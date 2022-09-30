31ea1a2c93b7f32c88cc0921c9bf8e4a0ec18dd9
nikhildeora <fitnexy@gmail.com>
2022-09-30 22:56:49 +0530

    car list page complete

---
+++ carlist.js
@@ -0,0 +1,468 @@
+
+
+let carData = [
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/b556e2f2c6f389b3793d0100529cde8f20d35be1.png?1663872428",
+        "Name": "chevrolet chevelle malibu",
+        "Star": "5.00",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 307,
+        "Km-drive": 18,
+        "Price": 3504,
+        "Km-away": 12,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/9044fb57f6c9d2e8d8f72dcf9244fdb4391c49fd.png?1663875640",
+        "Name": "buick skylark 320",
+        "Star": "4.50",
+        "Seats": "Manual - Diesal - 8 Seats",
+        "Popularity": 350,
+        "Km-drive": 15,
+        "Price": 3693,
+        "Km-away": 11.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/5d9078c7423169ed133f300b0d759b20ee488ab6.png?1663875987",
+        "Name": "plymouth satellite",
+        "Star": "4.75",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 318,
+        "Km-drive": 18,
+        "Price": 3436,
+        "Km-away": 11,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/3bdbccf3c1b15b1fb3adb2cc22ba237a5406c797.png?1663874667",
+        "Name": "amc rebel sst",
+        "Star": "3.00",
+        "Seats": "Automatic - Petrol - 8 Seats",
+        "Popularity": 304,
+        "Km-drive": 16,
+        "Price": 3433,
+        "Km-away": 12,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/f5c5ae8e660cfadcd93e9d4fcc087ab25e325bf9.png?1663874155",
+        "Name": "ford torino",
+        "Star": "4.00",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 302,
+        "Km-drive": 17,
+        "Price": 3449,
+        "Km-away": 10.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/a21d2ba781794d6c3f3e310c6a70b3d7c39cac89.png?1663876143",
+        "Name": "ford galaxie 500",
+        "Star": "3.50",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 429,
+        "Km-drive": 15,
+        "Price": 4341,
+        "Km-away": 10,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/8f34da6f17de55e93e00a6420d78a8655863e82c.png?1663871957",
+        "Name": "chevrolet impala",
+        "Star": "2.00",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 454,
+        "Km-drive": 14,
+        "Price": 4354,
+        "Km-away": 9,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/2733bd472b2c1cf2964cb9612854abbea8a9aeaa.png?1663874676",
+        "Name": "plymouth fury iii",
+        "Star": "3.80",
+        "Seats": "Automatic - Diesal - 8 Seats",
+        "Popularity": 440,
+        "Km-drive": 14,
+        "Price": 4312,
+        "Km-away": 8.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/388a2ee1a0dcc235b126c3f5cd204d560563a6a5.png?1663874258",
+        "Name": "pontiac catalina",
+        "Star": "3.45",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 455,
+        "Km-drive": 14,
+        "Price": 4425,
+        "Km-away": 10,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/2733bd472b2c1cf2964cb9612854abbea8a9aeaa.png?1663874676",
+        "Name": "amc ambassador dpl",
+        "Star": "3.30",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 390,
+        "Km-drive": 15,
+        "Price": 3850,
+        "Km-away": 8.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/5d35d1c9a1fcf7a450e92752da492f8876111437.png?1663872206",
+        "Name": "citroen ds-21 pallas",
+        "Star": "4.60",
+        "Seats": "Manual - Petrol - 4 Seats",
+        "Popularity": 133,
+        "Km-drive": 56,
+        "Price": 3090,
+        "Km-away": 17.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/2733bd472b2c1cf2964cb9612854abbea8a9aeaa.png?1663874676",
+        "Name": "chevrolet chevelle concours (sw)",
+        "Star": "5.00",
+        "Seats": "Manual - Diesal - 8 Seats",
+        "Popularity": 350,
+        "Km-drive": 65,
+        "Price": 4142,
+        "Km-away": 11.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/4ce151fca9d2de514fcec42977b2689e10bdff7d.png?1663871983",
+        "Name": "ford torino (sw)",
+        "Star": "4.94",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 351,
+        "Km-drive": 43,
+        "Price": 4034,
+        "Km-away": 11,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/13ac1f9054efd40b705ee8d11b603fcee4c9e6f8.png?1663874398",
+        "Name": "plymouth satellite (sw)",
+        "Star": "4.80",
+        "Seats": "Manual - Diesal - 8 Seats",
+        "Popularity": 383,
+        "Km-drive": 59,
+        "Price": 4166,
+        "Km-away": 10.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/5ef36d2a7a54280f15d4ef1d5af8a3c452838f08.png?1663874575",
+        "Name": "amc rebel sst (sw)",
+        "Star": "3.20",
+        "Seats": "Automatic - Petrol - 8 Seats",
+        "Popularity": 360,
+        "Km-drive": 23,
+        "Price": 3850,
+        "Km-away": 11,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/036dd29b96a1232243eb30bee11dc18445c19468.png?1663872961",
+        "Name": "dodge challenger se",
+        "Star": "4.00",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 383,
+        "Km-drive": 15,
+        "Price": 3563,
+        "Km-away": 10,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/36c99099965606701f7279bf525341fb161b5b2c.png?1663874435",
+        "Name": "plymouth 'cuda 340",
+        "Star": "5.00",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 340,
+        "Km-drive": 14,
+        "Price": 3609,
+        "Km-away": 8,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/019e8e3d1cd6e922b2b9ff4b147c2f5d4a377268.png?1663872490",
+        "Name": "ford mustang boss 302",
+        "Star": "4.70",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 302,
+        "Km-drive": 21,
+        "Price": 3353,
+        "Km-away": 8,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/a49559a67ba1d5fd7e2ffea2f78f654691ee018e.png?1663874345",
+        "Name": "chevrolet monte carlo",
+        "Star": "3.40",
+        "Seats": "Manual - Diesal - 8 Seats",
+        "Popularity": 400,
+        "Km-drive": 15,
+        "Price": 3761,
+        "Km-away": 9.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/5d9078c7423169ed133f300b0d759b20ee488ab6.png?1663875987",
+        "Name": "buick estate wagon (sw)",
+        "Star": "3.65",
+        "Seats": "Manual - Petrol - 8 Seats",
+        "Popularity": 455,
+        "Km-drive": 14,
+        "Price": 3086,
+        "Km-away": 10,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/f78e4971321153b67d0dedb3db409bc6b29aca4e.png?1663874062",
+        "Name": "toyota corona mark ii",
+        "Star": "3.00",
+        "Seats": "Manual - Petrol - 4 Seats",
+        "Popularity": 113,
+        "Km-drive": 24,
+        "Price": 2372,
+        "Km-away": 15,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/740dff9817b1e2553c584ce7b22d4deaffdd0b66.png?1663875879",
+        "Name": "plymouth duster",
+        "Star": "1.23",
+        "Seats": "Automatic - Petrol - 6 Seats",
+        "Popularity": 198,
+        "Km-drive": 22,
+        "Price": 2833,
+        "Km-away": 15.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/cebf4c75f7ec5fd92421779887f2cff1779d57e5.png?1663874523",
+        "Name": "amc hornet",
+        "Star": "2.50",
+        "Seats": "Manual - Diesal - 6 Seats",
+        "Popularity": 199,
+        "Km-drive": 18,
+        "Price": 2774,
+        "Km-away": 15.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/609e184935121f3dc0b92b4a7ac461f965c7576b.png?1663874146",
+        "Name": "ford maverick",
+        "Star": "2.13",
+        "Seats": "Manual - Petrol - 6 Seats",
+        "Popularity": 200,
+        "Km-drive": 21,
+        "Price": 2587,
+        "Km-away": 16,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/4ce151fca9d2de514fcec42977b2689e10bdff7d.png?1663871983",
+        "Name": "datsun pl510",
+        "Star": "4.00",
+        "Seats": "Manual - Petrol - 4 Seats",
+        "Popularity": 97,
+        "Km-drive": 27,
+        "Price": 2130,
+        "Km-away": 14.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/c636626530713d99c1ad9531d6a6fe24986b46b0.png?1663875952",
+        "Name": "volkswagen 1131 deluxe sedan",
+        "Star": "3.00",
+        "Seats": "Manual - Petrol - 4 Seats",
+        "Popularity": 97,
+        "Km-drive": 26,
+        "Price": 1835,
+        "Km-away": 20.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/84669243b5b9b47e7a5956951bb84a9237622ee2.png?1663876843",
+        "Name": "peugeot 504",
+        "Star": "4.00",
+        "Seats": "Manual - Petrol - 4 Seats",
+        "Popularity": 110,
+        "Km-drive": 25,
+        "Price": 2672,
+        "Km-away": 17.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/c636626530713d99c1ad9531d6a6fe24986b46b0.png?1663875952",
+        "Name": "audi 100 ls",
+        "Star": "4.98",
+        "Seats": "Manual - Diesal - 4 Seats",
+        "Popularity": 107,
+        "Km-drive": 24,
+        "Price": 2430,
+        "Km-away": 14.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/609e184935121f3dc0b92b4a7ac461f965c7576b.png?1663874146",
+        "Name": "saab 99e",
+        "Star": "5.00",
+        "Seats": "Automatic - Petrol - 4 Seats",
+        "Popularity": 104,
+        "Km-drive": 25,
+        "Price": 2375,
+        "Km-away": 17.5,
+    },
+    {
+        "Image": "https://zoomcar-assets.zoomcar.com/photographs/original/5f9e0fc31aa8107e2d487ce5756126703915c2e1.png?1663874214",
+        "Name": "bmw 2002",
+        "Star": "5.00",
+        "Seats": "Manual - Diesal - 4 Seats",
+        "Popularity": 121,
+        "Km-drive": 26,
+        "Price": 2234,
+        "Km-away": 12.5,
+    }]
+
+
+runAgain(carData)
+function runAgain(carData) {
+    carData.forEach(function (el) {
+        let tr = document.createElement("tr")
+        // td1 javascript 
+        let imgdiv = document.createElement("img")
+        imgdiv.src = el.Image
+        let td1 = document.createElement("td")
+        td1.append(imgdiv)
+
+        // td2 javascript 
+        let name = document.createElement("h2")
+        name.innerText = el.Name
+
+        let seats = document.createElement("p")
+        seats.innerText = el.Seats
+        let sign = document.createElement("p")
+        sign.innerText = "--"
+        let sign2 = document.createElement("p")
+        sign2.innerText = "--"
+        let ins = document.createElement("i")
+        ins.className = "fa-sharp fa-solid fa-star"
+        let symbol = document.createElement("h3")
+        symbol.append(ins)
+        let star = document.createElement("p")
+        star.innerText = el.Star
+        let popul = document.createElement("p")
+        popul.innerText = el.Popularity
+        let kmdr = document.createElement("p")
+        kmdr.innerText = el["Km-drive"]
+        let text = document.createElement("p")
+        text.innerText = "K Kms driven"
+        let divfor = document.createElement("div")
+        divfor.append(symbol, star, sign, popul, sign2, kmdr, text)
+
+        let td2 = document.createElement("td")
+        td2.append(name, seats, divfor)
+
+        // td3 javascript 
+        let kmaw = document.createElement("p")
+        kmaw.innerText = el["Km-away"]
+        let text2 = document.createElement("p")
+        text2.innerText = "Km away from you"
+        let td3 = document.createElement("td")
+        td3.append(kmaw, text2)
+
+        // td4 javascript 
+        let div2 = document.createElement("div")
+        let ins2 = document.createElement("p")
+        ins2.className = "fa-sharp fa-solid fa-indian-rupee-sign"
+        let symbol2 = document.createElement("h2")
+        symbol2.append(ins2)
+        let price = document.createElement("h2")
+        price.innerText = el.Price
+
+        div2.append(symbol2, price)
+        var a = document.createElement("a");
+        a.href = "carsummary.html";
+        a.innerText = "BOOK NOW";
+        a.addEventListener("click", function () {
+            carChoose(el)
+        })
+        let td4 = document.createElement("td")
+        td4.append(div2, a)
+
+
+        tr.append(td1, td2, td3, td4)
+        document.querySelector("tbody").append(tr)
+    })
+}
+
+
+
+function carChoose(el) {
+    localStorage.setItem("car-choose", JSON.stringify(el))
+}
+
+// javascript for fetching date 
+let data_trip = JSON.parse(localStorage.getItem("Round-Trip"))
+
+document.querySelector("#date>div:nth-child(1)").innerText = data_trip.place;
+document.querySelector("#date>div:nth-child(2)>span").innerText = data_trip.fromDate;
+document.querySelector("#date>div:nth-child(3)>span").innerText = data_trip.toDate;
+
+
+// sorting javascript 
+let copyData = carData.map(function (el) {
+    return el
+})
+
+document.getElementById("relevance").addEventListener("click", relevant)
+function relevant() {
+    document.querySelector("tbody").innerHTML = ""
+    runAgain(carData)
+}
+document.getElementById("low-to-high").addEventListener("click", lowtohigh)
+function lowtohigh() {
+    copyData.sort(function (a, b) {
+        return a.Price - b.Price;
+    })
+    document.querySelector("tbody").innerHTML = ""
+    runAgain(copyData)
+}
+document.getElementById("high-to-low").addEventListener("click", hightolow)
+function hightolow() {
+    copyData.sort(function (a, b) {
+        return b.Price - a.Price;
+    })
+    document.querySelector("tbody").innerHTML = ""
+    runAgain(copyData)
+}
+document.getElementById("best-rated").addEventListener("click", bestrated)
+function bestrated() {
+    copyData.sort(function (a, b) {
+        return Number(b.Star) - Number(a.Star);
+    })
+    document.querySelector("tbody").innerHTML = ""
+    runAgain(copyData)
+}
+document.getElementById("distance").addEventListener("click", distance)
+function distance() {
+    copyData.sort(function (a, b) {
+        return Number(a["Km-away"]) - Number(b["Km-away"]);
+    })
+    document.querySelector("tbody").innerHTML = ""
+    runAgain(copyData)
+}
+document.getElementById("car-age").addEventListener("click", carage)
+function carage() {
+
+}
+document.getElementById("kms-driven").addEventListener("click", kmsdriven)
+function kmsdriven() {
+    copyData.sort(function (a, b) {
+        return Number(a["Km-drive"]) - Number(b["Km-drive"]);
+    })
+    document.querySelector("tbody").innerHTML = ""
+    runAgain(copyData)
+}
+document.getElementById("popularity").addEventListener("click", popularity)
+function popularity() {
+    copyData.sort(function (a, b) {
+        return Number(b.Popularity) - Number(a.Popularity);
+    })
+    document.querySelector("tbody").innerHTML = ""
+    runAgain(copyData)
+}
+
+// search bar javascript 
+let searchBtn = document.querySelector("#search>button")
+searchBtn.addEventListener("click", searchValue)
+
+function searchValue() {
+    let gotVal = document.getElementById("searchBar").value
+    let myVal = copyData.filter(function (el) {
+        if (el.Name == gotVal) {
+            return el
+        }
+    })
+    document.getElementById("searchBar").value = ""
+    document.querySelector("tbody").innerHTML = ""
+    runAgain(myVal)
+}
